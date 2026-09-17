e2ab30ff550db613e16f7d02fe516ed1122e81b4
Nair Alves <nair.alves@uphold.com>
2026-09-17 16:30:15 +0100

    Apply restructured payment widget e2e options

--- projects/widget-test-app/src/pages/payment-widget/payment-widget.tsx
+++ projects/widget-test-app/src/pages/payment-widget/payment-widget.tsx
@@ -38,48 +38,38 @@ const FLOW_BUTTONS: FlowButton[] = [
   { flow: 'select-for-withdrawal', label: 'Select for Withdrawal' },
   {
     flow: 'deposit',
-    label: 'E2E Deposit (w/ Asset opt)',
+    label: 'E2E Deposit (w/ asset opt)',
     options: {
-      e2e: {
-        asset: 'ACN',
-        fees: { display: 'only-total' },
-        presetAmountsByCurrencyCode: { GBP: [5, 10, 15, 20], USD: [5, 10, 15, 20] }
-        // targetAccountId: 'ad123a42-ee77-448d-91c7-5d17b7362561'
+      amountSuggestions: { GBP: [5, 10, 15, 20], USD: [5, 10, 15, 20] },
+      availableAssets: { include: ['GBP', 'BTC'] },
+      fees: { display: 'total' },
+      initialSelection: {
+        destination: { asset: 'BTC' }
       }
     }
   },
 
   {
     flow: 'deposit',
-    label: 'E2E Deposit (w/ targetAccountId opt)',
+    label: 'E2E Deposit (w/ accountId opt)',
     options: {
-      e2e: {
-        // asset: 'BTC',
-        fees: { display: 'only-total' },
-        presetAmountsByCurrencyCode: { GBP: [5, 10, 15, 20], USD: [5, 10, 15, 20] },
-        targetAccountId: 'ad123a42-ee77-448d-91c7-5d17b7362561-----error'
-      }
-    }
-  },
-  {
-    flow: 'deposit',
-    label: 'E2E Deposit (w/ all fees opt)',
-    options: {
-      e2e: {
-        fees: { display: 'all' },
-        presetAmountsByCurrencyCode: { GBP: [5, 10, 15, 20], USD: [5, 10, 15, 20] },
-        targetAccountId: 'ad123a42-ee77-448d-91c7-5d17b7362561'
+      amountSuggestions: { GBP: [5, 10, 15, 20], USD: [5, 10, 15, 20] },
+      availableAssets: { include: ['GBP', 'BTC'] },
+      fees: { display: 'breakdown' },
+      initialSelection: {
+        destination: { accountId: 'c83373fd-a0ec-41be-9d9f-37a04370bb69' }
       }
     }
   },
   {
     flow: 'withdrawal',
-    label: 'E2E Withdraw (w/ targetAccountId opt)',
+    label: 'E2E Withdraw (w/ accountId opt)',
     options: {
-      e2e: {
-        fees: { display: 'only-total' },
-        presetAmountsByCurrencyCode: { GBP: [5, 10, 15, 20], USD: [5, 10, 15, 20] },
-        targetAccountId: 'ad123a42-ee77-448d-91c7-5d17b7362561'
+      amountSuggestions: { GBP: [5, 10, 15, 20], USD: [5, 10, 15, 20] },
+      // availableAssets: { include: ['GBP', 'BTC'] },
+      fees: { display: 'total' },
+      initialSelection: {
+        origin: { accountId: 'c83373fd-a0ec-41be-9d9f-37a04370bb69' }
       }
     }
   },
